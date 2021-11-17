e722717664e27cfea80b813b85aeee4d814768f3
anjirajardula <anjirajardula@gmail.com>
2021-11-17 08:56:12 +0530

    create contact details and test

---
+++ src/test/java/com/brdg/AddressBookMainTest.java
@@ -0,0 +1,13 @@
+package com.brdg;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookMainTest {
+    @Test
+    void givenDetailsTrue() {
+        Assertions.assertTrue(true);
+    }
+}
